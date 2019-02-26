591add28d493c7c80fd2d8145c2509534b4341db
rayGreycloud <rayGreycloud@gmail.com>
2019-02-26 12:32:22 -0800

    add lead action creator

--- leadmanager/frontend/src/actions/leadsActions.js
+++ leadmanager/frontend/src/actions/leadsActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_LEADS, DELETE_LEAD } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 export const getLeads = () => dispatch => {
   axios
@@ -21,6 +21,18 @@ export const deleteLead = id => dispatch => {
       dispatch({
         type: DELETE_LEAD,
         payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+export const addLead = data => dispatch => {
+  axios
+    .post('/api/leads/', data)
+    .then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
       });
     })
     .catch(err => console.log(err));
